ff05915979ffaef861aa583fc3232bfa1f4a0429
Devansh Arya <dv30arya@gmail.com>
2024-08-11 21:32:52 +0530

    commit

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'Topsis=topsis.topsis:main',
+            'Topsis=topsis.topsis_forcsv:main',
         ],
     },
     install_requires=[
